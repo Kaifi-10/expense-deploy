8724ceee410fb0fe7a9880106e04b3f13958df8d
Kaifi-10 <murghoobkaifi@gmail.com>
2024-07-07 12:19:09 +0530

    Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <div className={styles.title}>
+        Expense Tracker
+    </div>
+  )
+}
+
+export default Navbar
